09bd1676aa446d6704f000696165ab0dc0c0a7bd
AbdelrahmanHamdy1996 <46800696+AbdelrahmanHamdy1996@users.noreply.github.com>
2020-06-21 01:47:39 +0200

    Add file via upload

---
+++ StarRating.js
@@ -0,0 +1,23 @@
+class StarRating extends HTMLElement {
+    highlight(index) {
+        this.stars.forEach((star, i) => {
+            star.classList.toggle('full', i <= index);
+        });
+    }
+
+    constructor() {
+        super();
+
+        this.stars = [];
+
+        for (let i = 0; i < 5; i++) {
+            let s = document.createElement('div');
+            s.className = 'star';
+            this.appendChild(s);
+            this.stars.push(s)
+        }
+        this.highlight(2.5);
+    }
+}
+
+window.customElements.define('x-star-rating', StarRating);
