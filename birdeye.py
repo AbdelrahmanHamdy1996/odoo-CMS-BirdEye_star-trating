96caea5e7d6454aa3927c5fec95c09904357a701
Abdelrahman Hamdy <hamdyabdelrahman200@gmail.com>
2020-06-26 03:58:15 +0200

    deleted file

--- birdeye.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import Request, urlopen
-
-request = Request('https://api.birdeye.com/resources/v1/review/businessid/158170949468469/summary?api_key=bJQGlIUGx2ZZ9YlnuVPPYMVR3yhrmWAU')
-
-
-
-response_body = urlopen(request).read()
-print(response_body)
